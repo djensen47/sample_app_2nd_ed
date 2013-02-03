bf18bbcb26d42cdab8c219baf1a49596246d2fac
edwardupton <edward@teachable.co.uk>
2013-02-03 10:52:52 -0800

    Update app/assets/javascripts/keen_post.js

--- app/assets/javascripts/keen_post.js
+++ app/assets/javascripts/keen_post.js
@@ -1,13 +1,13 @@
 $(document).ready( function(){
 	Keen.addEvent("PageView",{page: document.title});
   	$('#new_micropost').submit(function() {
-		Keen.addEvent("Microblog",{post:true});
+		Keen.addEvent("Microblog",{"post":true});
 		console.log('keen post event tracked');
 		return;	
 	})
 	 
 	 $('#new_user').submit(function() {
-		Keen.addEvent("Signups",{signup:true});
+		Keen.addEvent("Signups",{"signup":true});
 		console.log('keen event tracked');
 		return;	
 	})
